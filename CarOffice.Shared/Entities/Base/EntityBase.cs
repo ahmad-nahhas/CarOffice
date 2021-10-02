0c96d4c1eba4f3c21d328239cebb2f710c9fc213
Ahmet Bakırcı <ahmetbakirci30@gmail.com>
2021-10-02 16:45:19 +0300

    some updates

--- CarOffice.Shared/Entities/Base/EntityBase.cs
+++ CarOffice.Shared/Entities/Base/EntityBase.cs
@@ -5,15 +5,15 @@ namespace CarOffice.Shared.Entities.Base
 {
     public abstract class EntityBase
     {
-        public Guid Id { get; set; }
-
-        [Display(Name = "Addition Date")]
-        public DateTime CreatedAt { get; set; }
-
         public EntityBase()
         {
             Id = Guid.NewGuid();
             CreatedAt = DateTime.Now;
         }
+
+        public Guid Id { get; set; }
+
+        [Display(Name = "Addition Date")]
+        public DateTime CreatedAt { get; set; }
     }
 }
